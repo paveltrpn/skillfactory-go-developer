a0a3e510025704de6d9de2c561371bdbd95a4abe
paveltrpn <paveltrpn@mail.ru>
2022-12-01 22:36:10 +0400

    refactor with select

--- cmd/ex29_6_2/main.go
+++ cmd/ex29_6_2/main.go
@@ -5,30 +5,32 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
-
-func handler(signal os.Signal) {
-	if signal == syscall.SIGINT {
-		fmt.Printf("Got CTRL+C signal, closing!\n")
-		os.Exit(0)
-	}
-}
 
 func main() {
 	sigchnl := make(chan os.Signal, 1)
+	caught := make(chan os.Signal, 1)
+
 	signal.Notify(sigchnl)
 
 	go func() {
 		for {
 			s := <-sigchnl
-			handler(s)
+			caught <- s
 		}
 	}()
 
-	var nbr int
-
-	for {
-		fmt.Scan(&nbr)
-		fmt.Printf("squared - %v\n", nbr*nbr)
+	for i := 0; ; i++ {
+		select {
+		case signal := <-caught:
+			if signal == syscall.SIGINT {
+				fmt.Printf("Got CTRL+C signal, closing!\n")
+				os.Exit(0)
+			}
+		default:
+			time.Sleep(time.Millisecond * 1000)
+			fmt.Println(i * i)
+		}
 	}
 }
